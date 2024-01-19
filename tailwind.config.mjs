4cbddd1e87f01cc5bc451a394387be3ebce803c8
francorinque <francoagustinrinquecorreo@gmail.com>
2024-01-19 13:18:48 -0300

    change buttons and bg

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,24 @@
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      text: {
+        ui: "text-slate-400",
+      },
+      animation: {
+        "background-shine": "background-shine 2s linear infinite",
+      },
+      keyframes: {
+        "background-shine": {
+          from: {
+            backgroundPosition: "0 0",
+          },
+          to: {
+            backgroundPosition: "-200% 0",
+          },
+        },
+      },
+    },
   },
   plugins: [],
 }
